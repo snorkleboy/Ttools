337226b6c418abbaab66b1d06394eeccab6da0d2
Tim Kharshan <timkharshan@hotmail.com>
2017-11-11 17:10:42 -0800

    minor fixes to installer

--- Ttools.sh
+++ Ttools.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #description installer for Ttools
-	  mkdir $HOME/testfoldeeer
+	  mkdir $HOME/bin
 	for file in *; do
 
 	  echo  moving ${file##*/} to $HOME/bin
 
-	  echo "`cp -u -r ${file} ./testfoldeeer`"
+	  echo "`cp -u -r ${file} $HOME/bin`"
 	done
